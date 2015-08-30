eb0e2a234d9d006c072dde02caed9ceae711aa4c
Tianon Gravi <admwiggin@gmail.com>
2015-08-30 10:41:08 -0700

    Add some TODO

--- src/cmd/gdbuild/main.go
+++ src/cmd/gdbuild/main.go
@@ -13,6 +13,8 @@ func main() {
 		log.Fatalf("usage: %s some/pkg.dsc|some/dir [...]\n", os.Args[0])
 	}
 
+	// TODO flags, etc
+
 	for _, arg := range os.Args[1:] {
 		fi, err := os.Stat(arg)
 		if err != nil {
@@ -20,6 +22,7 @@ func main() {
 		}
 		if fi.IsDir() {
 			buildSrc(arg)
+			// TODO extract the output and buildBin it
 		} else {
 			dsc, img := buildBin(arg)
 			fmt.Printf("\n- %q built in %q\n\n", dsc.Source, img)
